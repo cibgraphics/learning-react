1c262ee763c62300f6b0e328cfe4148cbe6d17b3
Christopher Bishop <cbishop@cibgraphics.com>
2017-06-27 08:06:07 -0600

    pull value from form

--- public/app.jsx
+++ public/app.jsx
@@ -1,19 +1,42 @@
 var Greeter = React.createClass({
-  getDefaultProps: function() {
+  getDefaultProps: function () {
     return {
       name: 'React',
       message: 'This is from the component',
     };
   },
+
+  getInitialState: function () {
+    return (
+      name: this.props.name
+    );
+  },
+
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    this.setState({
+      name: name
+    });
+
+  },
+
   render: function() {
 
-    var name = this.props.name,
+    var name = this.state.name,
         message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name}!</h1>
         <p>{message + "!!"}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
