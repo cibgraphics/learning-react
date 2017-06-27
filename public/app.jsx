153df9eccbcd9e7d79b42b83c7ff6ad54d781fed
Christopher Bishop <cbishop@cibgraphics.com>
2017-06-27 07:30:07 -0600

    default items for message and name prop

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,28 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is from the component',
+    };
+  },
   render: function() {
+
+    var name = this.props.name,
+        message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello {name}!</h1>
+        <p>{message + "!!"}</p>
       </div>
     );
   }
 });
 
+var firstName = "Chris",
+      message = "Message from Prop";
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={message} />,
   document.getElementById('app')
 );
