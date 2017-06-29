fbc8abde75f531e1a73eaf3f1579bf86e83cea0b
Christopher Bishop <cbishop@cibgraphics.com>
2017-06-29 08:01:27 -0600

    created nested components in prep for wireup

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,25 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Some h1!</h1>
+        <p>Some Paragraph</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  render: function() {
+    return (
+      <form>
+        <input type="text" ref="name" />
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
@@ -30,13 +52,19 @@ var Greeter = React.createClass({
 
     return (
       <div>
-        <h1>Hello {name}!</h1>
+
+        <h1>Hello {name}</h1>
         <p>{message + "!!"}</p>
+
+        <GreeterMessage />
 
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name" />
           <button>Set Name</button>
         </form>
+
+        <GreeterForm />
+
       </div>
     );
   }
